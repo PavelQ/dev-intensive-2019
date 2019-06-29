cd44816bf31029e6f079572ce784cacecc486e32
Pavel Kuleshov <pikuleshov@gmail.com>
2019-06-29 20:28:23 +0300

    Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -55,8 +55,61 @@ data class User(
                 lastId.toString(),
                 firstName = firstName,
                 lastName = lastName
-            );
+            )
         }
+    }
+
+     class Builder(
+        var id: String = "-1",
+        var firstName: String? = null,
+        var lastName: String? = null,
+        var avatar: String? = null,
+        var rating: Int = 0,
+        var respect: Int = 0,
+        var lastVisit: Date? = null,
+        var isOnline: Boolean = false
+    ) {
+
+        fun id(id: String): Builder {
+            this.id = id
+            return this
+        }
+        fun firstName(firstName: String?): Builder {
+            this.firstName = firstName
+            return this
+        }
+        fun lastName(lastName: String?): Builder {
+            this.lastName = lastName
+            return this
+        }
+        fun avatar(avatar: String?): Builder {
+            this.avatar = avatar
+            return this
+        }
+        fun rating(rating: Int): Builder {
+            this.rating = rating
+            return this
+        }
+        fun respect(respect: Int): Builder {
+            this.respect = respect
+            return this
+        }
+        fun lastVisit(lastVisit: Date?): Builder {
+            this.lastVisit = lastVisit
+            return this
+        }
+        fun isOnline(isOnline: Boolean): Builder {
+            this.isOnline = isOnline
+
+            return this
+        }
+
+        fun build():User{
+            return User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
+        }
+
+
+
     }
 
 }
