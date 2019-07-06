f61eb7cf483d04cd05a94fd433636716f2c3b1d6
Pavel Kuleshov <pikuleshov@gmail.com>
2019-07-06 20:02:43 +0300

    stripHtml method

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -15,3 +15,17 @@ fun String.truncate(len: Int = 16): String {
     }
     return subs
 }
+
+/**
+Реализуй extension позволяющий очистить строку от html тегов и html escape последовательностей ("& < > ' ""),
+а так же удалить пустые символы (пробелы) между словами если их больше 1. Необходимо вернуть модифицированную строку
+Пример:
+"<p class="title">Образовательное IT-сообщество Skill Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch
+"<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml() //Образовательное IT-сообщество Skill Branch
+ */
+
+fun String.stripHtml(): String {
+    val withoutHtml = "(<[^>]*>|[&'\"])".toRegex().replace(this, "")
+    val withouBigSpaces = " {2,}".toRegex().replace(withoutHtml, " ")
+    return withouBigSpaces
+}
